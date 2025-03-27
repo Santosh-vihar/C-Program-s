8666d81b13ed51cc7b72ae60aeb20c5ea4a8cfb5
Santosh-vihar <santoshvihar06@gmail.com>
2025-03-27 19:28:47 +0530

    Create For loop.c

---
+++ For loop.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    char number[9][6]={"one","two","three","four","five","six","seven","eight","nine"};
+    int i;
+    for (i=a;i<=b;i++){
+        if(i>=1 && i<=9)
+        printf("%s\n",number[i-1]);
+        else if(i>9){
+            if(i%2==0)
+                printf("even\n");
+            else
+                printf("odd\n");
+        }
+    }
+    return 0;
+}
