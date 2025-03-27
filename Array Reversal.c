0bf3bcd08e47fe3f8611cb19cf995b35832a430d
Santosh-vihar <santoshvihar06@gmail.com>
2025-03-27 19:28:01 +0530

    Create Array Reversal.c

---
+++ Array Reversal.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    reverseArray(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
